4327ef797543f8890641f1db3dd7ea1378e2d2ea
Kevin Jin <kjin@Kevins-MacBook-Pro-2.local>
2017-05-25 18:12:20 -0400

    copy

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# thvln
+# THavalon
+An extended ruleset for the Resistance: Avalon 
+
+## Roles
+
+### Good (Loyal Servants of Arthur) 
+The goal of Good players is to have three missions succeed, and then have Merlin and the Lovers survive the Assassination round. 
+
+#### Merlin 
+Merlin's role is largely unchanged from the base rules of Avalon. They are shown a list of players with Evil roles, as detailed below. 
+
+- Merlin correctly sees players with the following Evil roles as Evil: Lancelot (Evil), Morgana, Agravaine, Colgrevance, Oberon. 
+- Merlin incorrectly sees players with the following Good roles as Evil: Lancelot (Good)
+- Merlin incorrectly sees players with the following Evil role as Good: Mordred 
+
+Merlin is a valid target for Assassination. 
+
+#### Percival
+Percival's role is unchanged from the base rules of Avalon. They know which players have the Merlin or Morgana roles, but not which role is possessed by each player. 
+
+- Percival correctly sees Merlin as Merlin
+- Percival incorrectly sees Morgana as Merlin 
+
+#### Tristan and Iseult 
+Tristan and Iseult ("The Lovers") are two Good roles that provide their associated players with the knowledge that the other is also Good. 
+
+The Lovers are a valid target for Assassination.  
+
+#### Lancelot (Good)
+If Lancelot is in the game, he can be good or evil. If Good Lancelot is in the game, he is seen incorrectly by Merlin as evil. Good Lancelot can play reversal cards,
+which reverse the result of the mission he is on. A single reversal among passes will not fail a mission that requires 2 fails. Good Lancelot sees no one.
+
+#### Guinevere
+Guinevere knows her two lovers, Arthur and Evil Lancelot, but cannot distinguish between them.
+
+- Guinevere sees Arthur as her lover
+- Guinevere sees Lancelot (Evil) as her lover
+
+#### Arthur 
+Arthur is the only Good character to possess an activated ability. Arthur knows which other good roles are present in the game, but has no information about which players possess any of the roles present. Furthermore, Arthur has an ability, called Redemption, which allows Good one final chance at victory. 
+
+##### Ability: Redemption 
+If three of the first four missions fail, the player who has the Arthur role may formally declare as Arthur. You may, after consulting the other players, attempt to identify all evil players in the game. If you are correct, then the assassination round occurs as if three missions had succeeded; should the evil team fail to assassinate a viable target, the good team wins.
+
+### Evil (Minions of Mordred) 
+The goal of Evil players is to have either three missions fail, or should Good have won, assassinate either Merlin or the Lovers. 
+
+#### Mordred
+Mordred's role is unchanged from the base rules of Avalon. Mordred knows most of the other Evil team members, but is not seen by Merlin. 
+
+- Mordred correctly sees Agravaine, Colgrevance, Lancelot (Evil), and Morgana as Evil 
+- Mordred is not aware of Oberon
+- Mordred is incorrectly seen by Merlin as Good 
+
+#### Morgana 
+Morgana's role is unchanged from the base rules of Avalon. Morgana knows most of the other Evil team members, but is also seen incorrectly by Percival as Merlin. 
+
+- Morgana correctly sees Agravaine, Colgrevance, Lancelot (Evil), and Mordred as Evil 
+- Morgana is not aware of Oberon
+- Morgana is correctly seen by Merlin as Evil 
+- Morgana is incorrectly seen by Percival as Merlin 
+
+#### Lancelot (Evil) 
+Lancelot (Evil) is an Evil role that can be in the game if Good Lancelot is not. Evil Lancelot can play reversal cards, which reverse result of the missions he is on.
+A single reversal among passes will not fail a mission that requires 2 fails, but a fail and a reversal will.
+
+- Evil Lancelot correctly sees Agravaine, Colgrevance, Morgana, and Mordred as Evil 
+- Evil Lancelot is not aware of Oberon
+- Evil Lancelot is correctly seen by Merlin as Evil 
+
+#### Oberon 
+Oberon's role is significantly changed from the base rules of Avalon. Oberon is an Evil role that can be present in games with 7 or more players; however, Oberon cannot present in 9 player games. Oberon knows who else is Evil, but other Evil players do not generally know who Oberon is, although they will know that there is an Oberon (by the lack of a visible Evil character). Oberon also knows which Assassination targets are present in the game.
+
+- Oberon correctly sees Agravaine, Colgrevance, Lancelot (Evil), Mordred, and Morgana as Evil
+- Oberon is not seen by other Evil characters. 
+- Oberon is correctly seen by Merlin as Evil 
+- Oberon knows if there is a Merlin in the game
+- Oberon knows if both Tristan and Iseult are in the game 
+
+x`To counteract these significant drawbacks, Oberon posseses a unique ability, Betrayal. 
+
+##### Ability: Betrayal 
+After the cards from the 4th mission have been revealed, Oberon may formally declare as Oberon and cause the mission to fail, even if it would have succeeded. This ability is only usable if Oberon was on the 4th mission team. 
+
+#### Agravaine 
+Agravaine is an Evil role that can be present in games with 7 or more players. In addition to knowing the other visible Evil roles (e.g. all Evil except Oberon), Agravaine may single-handedly fail the fourth mission, via the Betrayal ability. 
+
+- Agravaine correctly sees Colgrevance, Lancelot (Evil), Mordred, and Morgana as Evil 
+- Agravaine must play a Fail card whenever they are sent on a mission. 
+
+#### Colgrevane 
+Colgrevance is an Evil role that can be present in games with 10 players. Colgrevance knows, precisely, the roles of all other Evil characters in the game. Thus, Colgrevane will be told not only who else is Evil, but which Evil role they possess. Note that Colgrevane *does* know who Oberon is, if Oberon is present in the game.  
---
+++ THavalon2.py
@@ -0,0 +1,346 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import shutil
+
+def main():
+	# get the number of players
+	num_players = int(input("How many people are playing?\n"))
+	if num_players < 5 or num_players > 10:
+		print("Invalid number of players")
+		exit(1)
+	
+	# get the names of players
+	players = set() # use as set to avoid duplicate players
+	for player_num in range(1, num_players+1):
+		players.add(input("Who is player " + str(player_num) + "?\n"))
+	players = list(players) # convert to list
+	random.shuffle(players) # ensure random order, though set should already do that
+	if len(players) != num_players:
+		print("No duplicate player names")
+		exit(1)
+
+	# choose 3 players
+	three_players = random.sample(players, 3)
+
+	# first two propose for the first mission, last is starting player of second round
+	first_mission_proposers = three_players[:2]
+	second_mission_starter = three_players[2]
+
+	# assign the roles in the game
+	good_roles = ["Merlin", "Percival", "Tristan", "Iseult", "Lancelot"]
+	evil_roles = ["Mordred", "Morgana", "Maelegant"]
+
+
+	if num_players >= 7:
+		good_roles.append("Guinevere")
+		good_roles.append("Arthur")
+		evil_roles.append("Agravaine")
+		evil_roles.append("Colgrevance")
+		if num_players != 9:
+			evil_roles.append("Oberon")
+
+	if num_players == 10:
+		good_roles.append("Gawain")
+
+	# shuffle the roles
+	random.shuffle(good_roles)
+	random.shuffle(evil_roles)
+
+	# determine the number of roles in the game
+	if num_players == 10:
+		num_evil = 4
+		num_good = 6
+	elif num_players == 9:
+		num_evil  = 3
+		num_good = 4
+	elif num_players == 7 or num_players == 8:
+		num_evil = 3
+		num_good = num_players - num_evil
+	else: # 5 or 6
+		num_evil = 2
+		num_good = num_players - num_evil
+
+	# assign players to teams
+	assignments = {}
+	reverse_assignments = {}
+	good_roles_in_game = set()
+	evil_roles_in_game = set()
+
+	if num_players == 9:
+		pelinor = players[8]
+		assignments[pelinor] = "Pelinor"
+		reverse_assignments["Pelinor"] = pelinor
+
+		questing_beast = players[7]
+		assignments[questing_beast] = "Questing Beast"
+		reverse_assignments["Questing Beast"] = questing_beast
+
+	good_players = players[:num_good]
+	evil_players = players[num_good:num_good + num_evil]
+
+
+	# assign good roles
+	for good_player in good_players:
+		player_role = good_roles.pop()
+		assignments[good_player] = player_role
+		reverse_assignments[player_role] = good_player
+		good_roles_in_game.add(player_role)
+
+	# assign evil roles
+	for evil_player in evil_players:
+		player_role = evil_roles.pop()
+		assignments[evil_player] = player_role
+		reverse_assignments[player_role] = evil_player
+		evil_roles_in_game.add(player_role)
+
+	# delete and recreate game directory
+	if os.path.isdir("game"):
+		shutil.rmtree("game")
+	os.mkdir("game")
+
+	# make every role's file
+
+	# Merlin sees: Morgana, Maelegant, Oberon, Agravaine, Colgrevance, Lancelot* as evil 
+	if "Merlin" in good_roles_in_game:
+		# determine who Merlin sees
+		seen = []
+		for evil_player in evil_players:
+			if assignments[evil_player] != "Mordred":
+				seen.append(evil_player)
+		if "Lancelot" in good_roles_in_game:
+			seen.append(reverse_assignments["Lancelot"])
+		random.shuffle(seen)
+
+		# and write this info to Merlin's file
+		player_name = reverse_assignments["Merlin"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Merlin.\n")
+			for seen_player in seen:
+				file.write("You see " + seen_player + " as evil.\n")
+
+	# Percil sees Merlin, Morgana* as Merlin
+	if "Percival" in good_roles_in_game:
+		# determine who Percival sees
+		seen = []
+		if "Merlin" in good_roles_in_game:
+			seen.append(reverse_assignments["Merlin"])
+		if "Morgana" in evil_roles_in_game:
+			seen.append(reverse_assignments["Morgana"])
+		random.shuffle(seen)
+
+		# and write this info to Percival's file
+		player_name = reverse_assignments["Percival"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Percival.\n")
+			for seen_player in seen:
+				file.write("You see " + seen_player + " as Merlin (or is it...?).\n")
+
+	if "Tristan" in good_roles_in_game:
+		# write the info to Tristan's file
+		player_name = reverse_assignments["Tristan"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Tristan.\n")
+			# write Iseult's info to file
+			if "Iseult" in good_roles_in_game:
+				iseult_player = reverse_assignments["Iseult"]
+				file.write(iseult_player + " is your lover.\n")
+			else: 
+				file.write("Nobody loves you. Not even your cat.\n")
+
+	if "Iseult" in good_roles_in_game:
+		# write this info to Iseult's file
+		player_name = reverse_assignments["Iseult"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Iseult.\n")
+			# write Tristan's info to file
+			if "Tristan" in good_roles_in_game:
+				tristan_player = reverse_assignments["Tristan"]
+				file.write(tristan_player + " is your lover.\n")
+			else: 
+				file.write("Nobody loves you.\n")
+
+	if "Lancelot" in good_roles_in_game: 
+		# write ability to Lancelot's file 
+		player_name = reverse_assignments["Lancelot"] 
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Lancelot. You are on the Good team. \n\n") 
+			file.write("Ability: Reversal \n")	
+			file.write("You are able to play Reversal cards while on missions. A Reversal card inverts the result of a mission; a mission that would have succeeded now fails and vice versa. \n \n")
+			file.write("Note: In games with at least 7 players, a Reversal played on the 4th mission results in a failed mission if there is only one Fail card, and otherwise succeeds. Reversal does not interfere with Agravaine's ability to cause the mission to fail.")
+
+	if "Guinevere" in good_roles_in_game:
+		# determine who Guinevere sees
+		seen = []
+		if "Arthur" in good_roles_in_game:
+			seen.append(reverse_assignments["Arthur"])
+		if "Lancelot" in good_roles_in_game:
+			seen.append(reverse_assignments["Lancelot"])
+		if "Maelegant" in evil_roles_in_game:
+			seen.append(reverse_assignments["Maelegant"])
+		random.shuffle(seen)
+
+		# and write this info to Guinevere's file
+		player_name = reverse_assignments["Guinevere"]
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Guinevere.\n")
+			for seen_player in seen:
+				file.write("You see " + seen_player + " as either Lancelot, Maelegant, or Arthur.\n")
+
+	if "Arthur" in good_roles_in_game:
+		# determine which roles Arthur sees
+		seen = []
+		for good_role in good_roles_in_game:
+			seen.append(good_role)
+		random.shuffle(seen)
+
+		# and write this info to Arthur's file
+		player_name = reverse_assignments["Arthur"]
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Arthur.\n\n")
+			file.write("The following good roles are in the game:\n")
+			for seen_role in seen:
+				if seen_role != "Arthur":
+					file.write(seen_role + "\n")
+			file.write("\n")
+			file.write("Ability: Redemption\n")
+			file.write("If three of the first four missions fail, you may reveal that you are Arthur. You may, after consulting the other players, attempt to identify all evil players in the game. If you are correct, then the assassination round occurs as if three missions had succeeded; should the evil team fail to assassinate a viable target, the good team wins.\n")
+
+	if "Gawain" in good_roles_in_game: 
+		# determine what Gawain sees 
+		seen = []
+		player_name = reverse_assignments["Gawain"]
+		good_players_no_gawain = set(good_players) - set([player_name])
+		# guaranteed see a good player
+		seen_good = random.sample(good_players_no_gawain, 1)
+		seen.append(seen_good[0])
+
+		# choose two other players randomly
+		remaining_players = set(players) - set([player_name]) - set(seen_good)
+		seen += random.sample(remaining_players, 2)
+
+		random.shuffle(seen)
+		
+		# write info to Gawain's file 
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Gawain.\n\n")
+			file.write("The following players are not all evil:\n")
+			for seen_player in seen:
+				file.write(seen_player + "\n")
+			file.write("\nAbility: Whenever a mission (other than the 1st) is sent, you may declare as Gawain to reveal a single person's played mission card. The mission card still affects the mission. (This ability functions identically to weak Inquisition and occurs after regular Inquisitions.) If the card you reveal is a Success, you are immediately 'Exiled' and may not go on missions for the remainder of the game, although you may still vote and propose missions.\n\n")
+			file.write("You may use this ability once per mission as long as you are neither on the mission team nor 'Exiled'. You may choose to not use your ability on any round, even if you would be able to use it.\n");
+
+
+	# make list of evil players seen to other evil
+	if "Oberon" in evil_roles_in_game:
+		evil_players = list(set(evil_players) - set([reverse_assignments["Oberon"]]))
+	random.shuffle(evil_players)
+
+	if "Mordred" in evil_roles_in_game:
+		player_name = reverse_assignments["Mordred"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Mordred. (Join us, we have jackets and meet on Thursdays. ~ Andrew and Kath)\n")
+			for evil_player in evil_players:
+				if evil_player != player_name:
+					file.write(evil_player + " is a fellow member of the evil council.\n")
+
+	if "Morgana" in evil_roles_in_game:
+		player_name = reverse_assignments["Morgana"]
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Morgana.\n")
+			for evil_player in evil_players:
+				if evil_player != player_name:
+					file.write(evil_player + " is a fellow member of the evil council.\n")
+
+	if "Oberon" in evil_roles_in_game:
+		player_name = reverse_assignments["Oberon"]
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Oberon.\n")
+			for evil_player in evil_players:
+				file.write(evil_player + " is a fellow member of the evil council.\n")
+			file.write("\nAbility: Should any mission get to the last proposal of the round, after the people on the mission have been named, you may declare as Oberon to replace one person on that mission with yourself.\n\n")
+			file.write("Note: You may not use this ability after two missions have already failed. Furthermore, you may only use this ability once per game.\n")
+
+	if "Agravaine" in evil_roles_in_game:
+		player_name = reverse_assignments["Agravaine"]
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Agravaine.\n")
+			for evil_player in evil_players:
+				if evil_player != player_name:
+					file.write(evil_player + " is a fellow member of the evil council.\n")
+			file.write("\nAbility: On any mission you are on, after the mission cards have been revealed, should the mission not result in a Fail (such as via a Reversal, requiring 2 fails, or other mechanics), you may formally declare as Agravaine to force the mission to Fail anyway.\n\n");
+			file.write("Drawback: You may only play Fail cards while on missions.\n");
+
+	if "Maelegant" in evil_roles_in_game: 
+		# write ability to Lancelot's file 
+		player_name = reverse_assignments["Maelegant"] 
+		filename = "game/" + player_name + ".txt"
+		with open(filename, "w") as file:
+			file.write("You are Maelegant. You are on the Evil team. \n\n") 
+			for evil_player in evil_players:
+				if evil_player != player_name:
+					file.write(evil_player + " is a fellow member of the evil council.\n")
+			file.write("\nAbility: Reversal \n")	
+			file.write("You are able to play Reversal cards while on missions. A Reversal card inverts the result of a mission; a mission that would have succeeded now fails and vice versa. \n \n")
+			file.write("Note: In games with at least 7 players, a Reversal played on the 4th mission results in a failed mission if there is only one Fail card, and otherwise succeeds. Reversal does not interfere with Agravaine's ability to cause the mission to fail.")
+
+	if "Colgrevance" in evil_roles_in_game:
+		player_name = reverse_assignments["Colgrevance"]
+		filename = "game/" + player_name
+		with open(filename, "w") as file:
+			file.write("You are Colgrevance.\n")
+			for evil_player in evil_players:
+				if evil_player != player_name:
+					file.write(evil_player + " is " + assignments[evil_player] + ".\n")
+			if "Oberon" in evil_roles_in_game:
+				file.write(reverse_assignments["Oberon"] + " is Oberon.\n")
+
+	# TODO: pelinor + questing beast
+	if num_players == 9:
+		# write pelinor's information
+		pelinor_filename = "game/" + pelinor
+		with open(pelinor_filename, "w") as file:
+			file.write("You are Pelinor.\n\n")
+			file.write("You must fulfill two of the following conditions to win:\n")
+			file.write("[1]: If Good wins via three mission success.\n")
+			file.write("[2]: If you go on the last mission with the Questing Beast.\n")
+			file.write("[3]: If, after the Assassination Round, you can guess the Questing Beast.\n")
+
+		questing_beast_filename = "game/" + questing_beast
+		with open(questing_beast_filename, "w") as file:
+			file.write("You are the Questing Beast.\n")
+			file.write("You must play the 'Questing Beast was here' card on missions.\n\n")
+			file.write("You must fulfill exactly one (not both) of the following conditions to win:\n")
+			file.write("[1]: If Evil wins via three missions failing.\n")
+			file.write("[2]: You never go on a mission with Pelinor.\n\n")
+			file.write(pelinor + " is Pelinor.\n")
+
+
+	# write start file
+	with open("game/start", "w") as file:
+		file.write("The players proposing teams for the first mission are:\n")
+		for first_mission_proposer in first_mission_proposers:
+			file.write(first_mission_proposer + "\n")
+		file.write("\n" + second_mission_starter + " is the starting player of the 2nd round.\n")
+
+	# write do not open
+	with open("game/DoNotOpen", "w") as file:
+		file.write("Player -> Role\n")
+		for player in players:
+			file.write(player + " -> " + assignments[player] + "\n")
+
+if __name__ == "__main__":
+	main()
